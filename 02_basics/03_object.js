7ad6c15ba357ecf4c018e22e9be2356ce2dcd00f
VANSOLA ARTHKUMAR MANUBHAI <146409368+Arth9vansola@users.noreply.github.com>
2025-01-23 00:37:47 +0000

    this is object

---
+++ 02_basics/03_object.js
@@ -0,0 +1,39 @@
+const obj = {
+    name: 'John',
+    age: 25,
+    city: 'New York',
+    country: 'USA',
+    email: 'john@gmail.com',
+    phone: '1234567890',
+}
+
+console.log(Object.keys(obj)); // it will return the keys of the object
+console.log(obj.name)
+console.log(obj['email'])
+
+// using symbols in object for interview purpose
+
+const mySymbol = Symbol('mySymbol');
+
+const obj2 ={
+    [mySymbol]: 'Hello' // here [mySymbol] is a taken as a reference from above declared mySymbol
+}
+console.log(obj2[mySymbol]);
+
+obj["name"] = "Vansola";
+obj["name"] = "arth"
+// Object.freeze(obj); // it will freeze the object means you can't change the value of the object
+// obj["name"] = "kumar";
+console.log(obj);
+
+//function inside object
+
+obj.greet = function(){
+    console.log("Hello beta");
+}
+console.log(obj.greet());
+
+obj.greet2 = function(){
+    console.log(`Hello beta, ${this.name}`);
+}
+console.log(obj.greet2());
