d53926c986d5366674836ee43ecf9756a074a701
VANSOLA ARTHKUMAR MANUBHAI <146409368+Arth9vansola@users.noreply.github.com>
2025-01-25 05:38:14 +0000

    immediately invoked function expression

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,26 @@
+// immediately invoked function expression (IIFE)
+
+// IIFE is a function that is executed right after it is created. It is a design pattern which is also known as a Self-Executing Anonymous Function and contains two major parts: () () , 
+
+// The first part contains the anonymous function and the second part is used to call the function immediately.
+
+// it is used to avoid polluting the global scope and it can be used to create a private scope. and also due to the global scope inner function can't be polluted
+
+
+// unnamed iife
+(()=>{
+     console.log("my name is arth");
+})(); // always apply semicolon after IIFE because due to this another iife know that first iife where is ended.
+
+( (naam)=>{
+    console.log(`Roses are red,
+Violets are blue,
+JavaScript is fun,
+And so are you. Happy ${naam}`)
+})("arth");
+
+// named iife
+(function greet(){
+    console.log("hello, world");
+})();
+
