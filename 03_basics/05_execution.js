de88a1cd299f222626198657122aec516cc946f7
VANSOLA ARTHKUMAR MANUBHAI <146409368+Arth9vansola@users.noreply.github.com>
2025-01-28 16:04:32 +0000

    execution

---
+++ 03_basics/05_execution.js
@@ -0,0 +1,14 @@
+// there is two type of execution context in javascript
+// 1. Global Execution Context
+// 2. Functional Execution Context
+// and another is like (eval execution context) but it is not mostly asked in interviews
+
+// there is two phase in the execution context
+// 1. Memory Creation Phase
+// 2. Execution Phase
+
+
+// in real life when we do any program it path is like
+// 1.global execution context
+// 2. memory creation phase
+// 3. execution phase --> if there is any function then it will create again new memory creation phase and execution phase
