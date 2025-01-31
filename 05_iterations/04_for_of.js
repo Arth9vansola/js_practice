d169612e60cf4378abf02a065ede6ea5b946ea17
VANSOLA ARTHKUMAR MANUBHAI <146409368+Arth9vansola@users.noreply.github.com>
2025-01-31 03:39:12 +0000

    operations on array is important

--- 05_iterations/04_for_of.js
+++ 05_iterations/04_for_of.js
@@ -1,4 +1,4 @@
-// for of is used for "arrays" and "strings" mostly where we deal with index based iteration here we directly get values
+// for of is used for 🎲"arrays"🎲imp and "strings" mostly where we deal with index based iteration here we directly get values
 
 const arr = [1, 2, 3, 4, 5];
 for (let element of arr) {
