48838d91afebd410f9e9796bd03c9d1334048feb
VANSOLA ARTHKUMAR MANUBHAI <146409368+Arth9vansola@users.noreply.github.com>
2025-01-31 00:35:44 +0000

    this is do while loop

---
+++ 05_iterations/03_do_while_loop.js
@@ -0,0 +1,15 @@
+// do while loop
+
+let score = 50
+do {
+    console.log(`your score is ${score}`);
+    score++;
+} while (score < 60);
+
+
+
+let scape = 50
+do {
+    console.log(`your score is ${scape}`);
+    scape++;
+} while (scape > 60);
