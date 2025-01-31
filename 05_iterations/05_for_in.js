70b5ca631263e07c90c09691c4bfa24ffce4d048
VANSOLA ARTHKUMAR MANUBHAI <146409368+Arth9vansola@users.noreply.github.com>
2025-01-31 03:39:25 +0000

    about for in loop

---
+++ 05_iterations/05_for_in.js
@@ -0,0 +1,18 @@
+// for in loop ::::---> used for object and array, it give you direct keys of object and index of array
+
+// for object
+const obj = {
+    name: 'john',
+    age: 30,
+    job: 'web developer'
+}
+
+for (let key in obj) {
+    console.log(obj[key]);
+}
+
+//for array
+const arr = [1, 2, 3, 4, 5];
+for (let index in arr) {
+    console.log(arr[index]);
+}
