3210c3cae639d8308b86feae22d238d252d6904a
VANSOLA ARTHKUMAR MANUBHAI <146409368+Arth9vansola@users.noreply.github.com>
2025-01-31 03:39:40 +0000

    about for each loop

---
+++ 05_iterations/06_for_each.js
@@ -0,0 +1,31 @@
+// for each loop is also used for array but it give you direct value of array
+
+// way 1:
+const arr = [1, 2, 3, 4, 5];
+arr.forEach((val)=>(console.log(val))) // 1 2 3 4 5
+
+// way 2:
+const arr2 = [10,5,20,30,4];
+function print(val){
+    console.log(val)
+}
+// arr2.forEach(print) // 10 5 20 30 4
+
+// here foreach come with item, index and entire array
+const cars = ["Toyota", "Honda", "Ford", "BMW", "Audi"];
+cars.forEach((item, index, arr)=>{
+  console.log(item, index, arr);
+});
+
+// array of object
+const carCompanies = [
+    { company: "Toyota", model: "Supra" },
+    { company: "Honda", model: "NSX" },
+    { company: "Ford", model: "GT" },
+    { company: "BMW", model: "i8" },
+    { company: "Audi", model: "R8" }
+];
+
+carCompanies.forEach((item)=>{
+    console.log(item["company"], item["model"]);
+})
