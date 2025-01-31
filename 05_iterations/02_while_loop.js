0ed1c4cab59339e032b6b5fbe566b27ed0765656
VANSOLA ARTHKUMAR MANUBHAI <146409368+Arth9vansola@users.noreply.github.com>
2025-01-31 00:35:23 +0000

    this is while loop

---
+++ 05_iterations/02_while_loop.js
@@ -0,0 +1,14 @@
+// while loop
+
+let a = 0;
+while (a < 5) {
+    console.log(a);
+    a++;
+}
+
+let myArray = [1, 2, 3, 4, 5];
+let i = 0;
+while (i < myArray.length) {
+    console.log(`my given array element is ${myArray[i]}`);
+    i+=1;
+}
