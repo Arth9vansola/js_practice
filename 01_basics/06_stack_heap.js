7cf2edf30c0773b46c1a687ecc8235ac70c84cff
VANSOLA ARTHKUMAR MANUBHAI <146409368+Arth9vansola@users.noreply.github.com>
2025-02-02 05:11:19 +0000

    change here

--- 01_basics/06_stack_heap.js
+++ 01_basics/06_stack_heap.js
@@ -1,15 +1,15 @@
 // stack (primitive) ---> give the copy of the original value(call by value)
 
-let arth = "Vansola"
-let avi = arth
-avi = "vankar"
-console.log(arth);
-console.log(avi);
+let a = "Vansola"
+let b = a
+b = "vankar"
+console.log(a);
+console.log(b);
 
 // heap (non-primitive / reference) ---> give the reference of the original value(call by reference)
 
-let a = [1, 2, 3]
-let b = a
-b[2] = 50
-console.log(a);
-console.log(b);
+let x = [1, 2, 3]
+let y = x
+y[2] = 50
+console.log(x);
+console.log(y);
