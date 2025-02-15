a15f6cbd709e75c96ea84803d86e6f4e17b2f269
VANSOLA ARTHKUMAR MANUBHAI <146409368+Arth9vansola@users.noreply.github.com>
2025-02-15 05:17:33 +0000

    fetch

---
+++ 09_async_await_XmlHttpRequest/fetch/fetch.js
@@ -0,0 +1,33 @@
+const promiseFive = new Promise(function(resolve, reject){
+        setTimeout(()=>{
+            let er = true
+            if(!er){// falsy condition hogi to hi run hoga varna run nahi hoga when ! is there  in condition
+                resolve({name: "avinav", age: 21 , role: "admin"})
+            }else{
+                reject("daya kuch to gadbad hai")
+            }
+              
+        },3000)
+    })
+    
+    async function handlepromiseFive(){
+        try{
+              const data = await promiseFive
+              console.log(data)
+        }catch(err){
+            console.log(err)
+        }
+    }
+    handlepromiseFive()
+
+
+fetch('https://api.github.com/users/hiteshchoudhary') // here fetch come before the promiseFive because it has special micro task queue / priority queue which execurte fetch request first without waiting for the promiseFive to complete and this is the beauty of fetch
+.then((response)=>{
+    return response.json()
+})
+.then((data)=>{
+    console.log(data)
+})
+.catch((err)=>{
+    console.log(err)
+})
