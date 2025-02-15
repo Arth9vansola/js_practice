dd8f4d2532c9718c70682a561627475d877c3ff8
VANSOLA ARTHKUMAR MANUBHAI <146409368+Arth9vansola@users.noreply.github.com>
2025-02-15 06:00:41 +0000

    oop

---
+++ 10_oop/oop.js
@@ -0,0 +1,7 @@
+// js is prototype based language and classes are only syntactic sugar
+// In properties , we can not pass value .length(), .toUpperCase() etc
+// In methods(function), we can pass value map(func), filter(func) etc
+
+
+// 1)  when we create "new" it will create new empty object {}
+// 2)  new keyword call the constructor function which pack the arguments
